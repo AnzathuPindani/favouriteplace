459794491be7f459c59913b44db0ce26d0eb100d
AnzathuPindani <anzathu.pindani@nicotechnologies.com>
2023-05-18 06:58:35 +0200

    latitude location

--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -1,7 +1,9 @@
 import 'package:favourite_place_app/providers/user_places.dart';
 import 'package:favourite_place_app/widgets/image_input.dart';
+import 'package:favourite_place_app/widgets/location_input.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import "dart:io";
 
 class AddPlaceScreen extends ConsumerStatefulWidget {
   const AddPlaceScreen({super.key});
@@ -12,14 +14,20 @@ class AddPlaceScreen extends ConsumerStatefulWidget {
 
 class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
   final _titleController = TextEditingController();
+  File? _selectedImage;
 
   void _savePlace() {
     final enteredText = _titleController.text;
+    if (enteredText.isEmpty || _selectedImage == null) {
+      return;
+    }
 
     if (enteredText.isEmpty) {
       return;
     }
-    ref.read(userPlacesProvider.notifier).addPlace(enteredText);
+    ref
+        .read(userPlacesProvider.notifier)
+        .addPlace(enteredText, _selectedImage!);
     Navigator.of(context).pop();
   }
 
@@ -48,7 +56,15 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
               height: 10,
             ),
             // Image Input
-            ImageInput(),
+            ImageInput(
+              onPickImage: (image) {
+                _selectedImage = image;
+              },
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            LocationInput(),
             const SizedBox(
               height: 16,
             ),
